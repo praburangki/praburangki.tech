e841db33a1ea16cb21008ef6e2d2cec6a0de1f4b
praburangki <praburangki@gmail.com>
2019-01-28 16:23:55 +0800

    Adding images and another test for netlify

---
+++ pages/blogs.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  div
+    img.lazyload(src="https://farm6.staticflickr.com/5531/9638435181_7e3e44c2b8_b.jpg")
+</template>
+
+<style scoped>
+div {
+  margin-top: 1700px;
+  display: flex;
+  transition: all 0.1s;
+}
+</style>
