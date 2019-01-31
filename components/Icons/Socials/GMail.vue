3f1dc5843d95a034edf17ceee7d95a6bf4a4ecab
praburangki <praburangki@gmail.com>
2019-01-31 13:57:53 +0800

    fix typo in gmail

---
+++ components/Icons/Socials/GMail.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  svg(xmlns='http://www.w3.org/2000/svg', viewBox='0 0 512 512')
+    path(fill='#eceff1', d='M64 64h384v384H64z')
+    path(fill='#cfd8dc', d='M256 296.4L448 448V148.7z')
+    path(d='M464 64h-16L256 215.6 64 64H48a48 48 0 0 0-48 48v288a48 48 0 0 0 48 48h16V148.7l192 147.7 192-147.8V448h16a48 48 0 0 0 48-48V112a48 48 0 0 0-48-48z', fill='#f44336')
+</template>
