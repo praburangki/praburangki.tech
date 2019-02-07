595933631454c558a321e6964e0997ed5b899953
praburangki <praburangki@gmail.com>
2019-02-07 16:28:31 +0800

    add ability to toggle theme

--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,22 +1,57 @@
 <template lang="pug">
-  .container
-    h1.is-size-2.has-text-centered {{ post.title }}
-    DynamicMarkdown(
-      :renderFunc="post.renderFunc",
-      :staticRenderFuncs="post.staticRenderFuncs"
-    )
+  section(style="margin-bottom: 50px")
+    ToggleTheme(@toggleTheme="toggleTheme", :checked="theme === 'dark'")
+    .container
+      h1.is-size-2.has-text-centered {{ post.title }}
+      DynamicMarkdown(
+        :renderFunc="post.renderFunc",
+        :staticRenderFuncs="post.staticRenderFuncs"
+      )
 </template>
 
 <script>
 import DynamicMarkdown from '~/components/blogs/DynamicMarkdown';
+import ToggleTheme from '~/components/blogs/ToggleTheme';
 
 export default {
   components: {
-    DynamicMarkdown
+    DynamicMarkdown,
+    ToggleTheme
   },
+  data: () => ({
+    theme: null,
+    coba: 'hehh'
+  }),
   computed: {
     post() {
       return this.$store.getters.getPost(this.$route.params.slug);
+    }
+  },
+  created() {
+    this.theme = 'dark';
+  },
+  mounted() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      document.querySelector('body').classList.add('light');
+      this.theme = 'light';
+    }
+  },
+  beforeDestroy() {
+    document.querySelector('body').classList.remove('light');
+  },
+  methods: {
+    toggleTheme(e) {
+      const isChecked = e.target.checked;
+      const bodyClass = document.querySelector('body').classList;
+      this.theme = isChecked ? 'dark' : 'light';
+      if (isChecked) {
+        bodyClass.remove('light');
+        localStorage.removeItem('theme');
+      } else {
+        bodyClass.add('light');
+        localStorage.setItem('theme', 'light');
+      }
     }
   }
 };
